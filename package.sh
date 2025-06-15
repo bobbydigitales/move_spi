2bac53cee059c808fee0062d39138b843ed21b39
bobbydigitales <spam@anderberg.co.uk>
2025-06-14 23:00:29 -0700

    Fix the package script to only have one directory

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cp -vf ./control_surface_move move_m8.js move_default.js start_control_surface_move.sh stop_control_surface_move.sh ./build/control_surface_move
-tar -czvf control_surface_move.tar.gz ./build/control_surface_move
+tar -C ./build/ -czvf control_surface_move.tar.gz control_surface_move
