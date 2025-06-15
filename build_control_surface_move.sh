b4baf297dac20d5775d53e1a19e960b5feb4f35f
bobbydigitales <spam@anderberg.co.uk>
2025-06-14 20:37:00 -0700

    Add control_surface_move binary, package script and copy the whole build directory to the Move

--- build_control_surface_move.sh
+++ build_control_surface_move.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gcc -g control_surface_move.c -o control_surface_move -Llibs/quickjs/quickjs-2025-04-26/ -lquickjs -lm && ./copy.sh control_surface_move && scp move_m8.js ableton@move:./Projects/move_spi/ && scp move_default.js ableton@move:./Projects/move_spi/
+gcc -g control_surface_move.c -o control_surface_move -Llibs/quickjs/quickjs-2025-04-26/ -lquickjs -lm
+cp -vf ./control_surface_move move_m8.js move_default.js start_control_surface_move.sh stop_control_surface_move.sh ./build/control_surface_move
+
+scp  -r ./build/control_surface_move ableton@move.local:~/Projects/move_spi/build/
+# ./copy.sh control_surface_move
+# scp move_m8.js ableton@move:./Projects/move_spi/
+# scp move_default.js ableton@move:./Projects/move_spi/
